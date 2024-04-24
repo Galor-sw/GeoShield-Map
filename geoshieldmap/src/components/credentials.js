a62a9c2882253bc4543abf322952494021f99b52
Galor-sw <80087330+Galor-sw@users.noreply.github.com>
2024-04-24 12:14:03 +0300

    Update credentials.js

--- geoshieldmap/src/components/credentials.js
+++ geoshieldmap/src/components/credentials.js
@@ -1,22 +1,22 @@
 // Function to return AWS credentials (hardcoded)
 const getAWSCredentials = () => {
     return {
-        region: "eu-west-1",
+        region: "",
         credentials: {
-            accessKeyId: "AKIAZI2LD2CGWD47VQOM",
-            secretAccessKey: "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",
+            accessKeyId: "",
+            secretAccessKey: "",
         },
     };
 };
 
 // Function to return Google Maps API key (hardcoded)
 const getGoogleMapsApiKey = () => {
-    return "AIzaSyDdKQY_n89HWZDY7032fvrra6JrECnFAjU";
+    return "";
 };
 
 // Function to return hardcoded map ID
 const getMapId = () => {
-    return "your_map_id";
+    return "";
 };
 
 // Export the functions
